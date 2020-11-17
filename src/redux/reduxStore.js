49f8ae3f036a663036d75d665ee24be6f9a7d0f5
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-11-17 21:25:49 +0300

    Слетел Git

---
+++ src/redux/reduxStore.js
@@ -0,0 +1,11 @@
+// import {createStore, combineReducers, applyMiddleware} from 'redux';
+// import cardsPlaceReducer from './reducers/cardsPlaceReducer';
+// import thunk from 'redux-thunk';
+
+// const reducers = combineReducers({
+//     cardsPlace: cardsPlaceReducer
+// });
+
+// const store = createStore(reducers, applyMiddleware(thunk));
+
+// export default store;
